18c3e722b3171c8e74b6d908c1b06b7a6d9cf29c
linzi9205 <lwu@cca.edu>
2017-04-09 23:48:40 -0700

    Add files via upload

---
+++ hw9/embodiednontask/verbal.js
@@ -0,0 +1,126 @@
+/* verbal part */
+var state = "initial"
+var slowBreathInc = 0.1
+var fastBreathInc = 0.6
+var slowTimeBetweenBlinks = 4000
+var fastTimeBetweenBlinks = 500
+
+function startDictation() {
+
+  if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+    var recognition = new webkitSpeechRecognition();
+
+    /* Nonverbal actions at the start of listening */
+    setTimeBetweenBlinks(fastTimeBetweenBlinks);
+    setBreathInc(slowBreathInc);
+
+    recognition.continuous = false;
+    recognition.interimResults = false;
+
+    recognition.lang = "zh-CN";
+    recognition.start();
+
+
+    recognition.onresult = function(e) {
+      document.getElementById('transcript').value
+                               = e.results[0][0].transcript;
+      var user_said = e.results[0][0].transcript;
+      recognition.stop();
+
+      /* Nonverbal actions at the end of listening */
+      setTimeBetweenBlinks(slowTimeBetweenBlinks);
+      jump(); //perform a nonverbal action from nonverbal.js
+
+      var bot_response = decide_response(user_said)
+      speak(bot_response)
+
+      //`document.getElementById('labnol').submit();
+    };
+
+    recognition.onerror = function(e) {
+      recognition.stop();
+    }
+
+  }
+}
+
+/* decide what to say.
+ * input: transcription of what user said
+ * output: what bot should say
+ */
+function decide_response(user_said) {
+var response;
+    var play_re = /chang'ge\s(.+)/i;  // creating a regular expression
+    var play_parse_array = user_said.match(play_re) // parsing the input string with the regular expression
+
+    console.log(play_parse_array) // let's print the array content to the console log so we understand
+                                  // what's inside the array.
+
+    if (play_parse_array && state === "initial") {
+      response = "hao'de,zai'chang" + play_parse_array[1];
+    } else if (user_said.toLowerCase().includes("chang") && state === "initial") {
+      response = "chang'shen'me?";
+      state = "sing_song"
+    } else if (user_said.toLowerCase().includes("zai'jian")) {
+      response = "zai'jian";
+      state = "initial"
+    } else if (state === "sing_song") {
+      response = "hao'de,zai'chang" + user_said;
+      state = "initial"
+    } else {
+      response = "ni'zai'shuo'sha";
+    }
+    return response;
+}
+
+/* Load and print voices */
+function printVoices() {
+  // Fetch the available voices.
+  var voices = speechSynthesis.getVoices();
+  
+  // Loop through each of the voices.
+  voices.forEach(function(voice, i) {
+        console.log(voice.name)
+  });
+}
+
+printVoices();
+
+/* 
+ *speak out some text 
+ */
+function speak(text, callback) {
+
+  /* Nonverbal actions at the start of robot's speaking */
+  setBreathInc(fastBreathInc); 
+
+  console.log("Voices: ")
+  printVoices();
+
+  var u = new SpeechSynthesisUtterance();
+  u.text = text;
+  u.lang = 'zh-CN';
+  u.volume = 0.5 //between 0.1
+  u.pitch = 2.0 //between 0 and 2
+  u.rate = 1.0 //between 0.1 and 5-ish
+  u.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == "Ting-Ting"; })[0]; //pick a voice
+
+  u.onend = function () {
+      
+      /* Nonverbal actions at the end of robot's speaking */
+      setBreathInc(slowBreathInc); 
+
+      if (callback) {
+          callback();
+      }
+  };
+
+  u.onerror = function (e) {
+      if (callback) {
+          callback(e);
+      }
+  };
+
+  speechSynthesis.speak(u);
+}
